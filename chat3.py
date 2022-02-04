bc4524ecac0fe56434c0e0e6326f8a996145de0e
Elaine Wang <elaine35002@gmail.com>
2022-02-04 22:24:28 +0800

    add chat3.py

--- chat3.py
+++ chat3.py
@@ -5,7 +5,8 @@ with open('3.txt', 'r', encoding='utf-8-sig') as f:
         lines.append(line.strip())
 
 for line in lines:
-    s = line.split(' ')
-    time = s[0][:5]
-    name = s[0][5:]
+    s = line.split(' ')  #用空白去分割
+    time = s[0][:5]  # 開始值0 最後一個不包含 所以是01234  
+    name = s[0][5:]  # 從第五個開始走到結尾 (結尾值不寫就是走到結尾)
+    print(time)
     print(name)
